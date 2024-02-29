2ee4b0bb211e6e639cc3d56c380d6f453cc6811d
Maheshwarreddy <118247589+Maheshwarreddy970@users.noreply.github.com>
2024-02-29 23:13:57 +0530

    Update page.tsx

--- apps/web/src/app/dashboard/page.tsx
+++ apps/web/src/app/dashboard/page.tsx
@@ -1,24 +1,6 @@
 import Dashboard from '@/components/Dashboard'
-import { db } from '@repo/database/dbconnect'
-import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
-import { redirect } from 'next/navigation'
-import { UserType } from '@repo/trpc/types'
 
 const Page = async () => {
-  const { getUser } = getKindeServerSession()
-  const user = await getUser()
-
-  if (!user || !user || !user.id) redirect('/auth-callback?origin=dashboard')
-
-  const dbUser:UserType|null = await db.user.findFirst({
-    where: {
-      id: user.id
-    }
-  })
-
-  if(!dbUser) redirect('/auth-callback?origin=dashboard')
-
-
   return <Dashboard/>
 }
 
